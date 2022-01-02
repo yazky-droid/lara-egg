353f792059e0e5117dfa683223deb8a126788b46
Yazky Maulana <yazki.maulana@gmail.com>
2022-01-02 22:55:08 +0700

    add laravel mix and sass bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 8 | {{ $title }} @yield('title')</title>
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
     {{-- ini pake isset, isset adalah kondisi jika true jalankan jika tidak yaudah gitu ga ngelakuin apa-apa --}}
     {{-- @isset($styles)
     {{ $styles }}
@@ -18,6 +18,6 @@
     <div class="pt-4">
         {{ $slot  }}
     </div>
-    <script src="/js/bootstrap.min.css"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
